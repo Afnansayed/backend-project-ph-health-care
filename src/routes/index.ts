572b74cf1497e2314b0611c16e08635ef542bf11
Afnan Sayed <afnansayed1973@gmail.com>
2026-03-01 17:21:21 +0600

    feat: "add doctor route"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authRoutes } from "../app/module/auth/auth.route";
 import { specialtyRoute } from "../app/module/specialty/specialty.route";
 import { userRoute } from "../app/module/user/user.route";
+import { doctorRoutes } from "../app/module/doctor/doctor.route";
 
 
 
@@ -10,5 +11,6 @@ const router = Router();
 router.use("/auth", authRoutes);
 router.use("/specialty", specialtyRoute);
 router.use("/user", userRoute);
+router.use("/doctor", doctorRoutes);
 
 export const indexRoutes = router;
