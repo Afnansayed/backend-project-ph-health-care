f8a946910a6d1b716512e158c73cab02115e3dda
Afnan Sayed <afnansayed1973@gmail.com>
2026-03-02 14:54:50 +0600

    zod validaion error handle with middleware and error helper function

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createDoctorZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post('/create-doctor', userController.creteDoctor);
+router.post('/create-doctor', validateRequest(createDoctorZodSchema), userController.creteDoctor);
 
 export const userRoute = router;
