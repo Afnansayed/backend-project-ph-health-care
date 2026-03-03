45db82f2a3dac5a8af063fd5a0daeb2d9577e0cc
Afnan Sayed <afnansayed1973@gmail.com>
2026-03-03 18:10:14 +0600

    create checkAuth middleware for check is user are autherize or not

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express,{ Application, Request, Response } from "express";
 import { indexRoutes } from "./routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFoundHandler } from "./app/middleware/notFound";
+import cookieParser from "cookie-parser";
 
 const app:Application = express()
 // Enable URL-encoded form data parsing
@@ -9,12 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1", indexRoutes);
 
 // Basic route
 app.get('/', async (req: Request, res: Response) => {
-   res.send("wlcome to ph-healthcare backend");
+   res.send("welcome to ph-healthcare backend");
 });
 
 // Global error handler
