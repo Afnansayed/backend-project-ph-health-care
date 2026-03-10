91f85cc8ab7270285aa86cd0ff25c36b1b8aff2d
Afnan Sayed <afnansayed1973@gmail.com>
2026-03-10 23:22:20 +0600

    fix : "spelling"

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -4,12 +4,12 @@ import { checkAuth } from "../../middleware/checkAuth";
 import { Role } from "../../../generated/prisma/enums";
 import { multerUpload } from "../../config/multer.config";
 import { validateRequest } from "../../middleware/validateRequest";
-import { SpecialtyValidation } from "./specialty.validaton";
+import { SpecialtyValidation } from "./specialty.validation";
 
 const router = Router();
 
 router.post('/',
-// checkAuth(Role.ADMIN,Role.SUPER_ADMIN),
+checkAuth(Role.ADMIN,Role.SUPER_ADMIN),
 multerUpload.single("file"),
 validateRequest(SpecialtyValidation.createSpecialtyZodSchema),
 SpecialtyController.createSpecialty);
