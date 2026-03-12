e39bf31568ae8d6f9623c35bd56603065002d446
Afnan Sayed <afnansayed1973@gmail.com>
2026-03-13 00:30:38 +0600

    scedule

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { specialtyRoute } from "../app/module/specialty/specialty.route";
 import { userRoute } from "../app/module/user/user.route";
 import { doctorRoutes } from "../app/module/doctor/doctor.route";
 import { AdminRoutes } from "../app/module/admin/admin.route";
+import { scheduleRoutes } from "../app/module/schedule/schedule.route";
 
 
 
@@ -14,5 +15,6 @@ router.use("/specialty", specialtyRoute);
 router.use("/user", userRoute);
 router.use("/doctor", doctorRoutes);
 router.use('/admin', AdminRoutes);
+router.use('/schedule', scheduleRoutes);
 
 export const indexRoutes = router;
